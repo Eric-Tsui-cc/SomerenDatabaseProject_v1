0a3e9d13c48526531c0dfcd146fefd09a0f858c5
Edgerunner0 <699380@student.inholland.nl>
2024-03-13 11:42:04 +0100

    create teacherDao

---
+++ SomerenDAL/TeacherDao.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SomerenModel;
+
+namespace SomerenDAL
+{
+    public class TeacherDao : BaseDao
+    {
+        public List<Teacher> GetAllTeachers()
+        {
+            string query = "SELECT lecturerid, Name FROM LECTURER";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        private List<Teacher> ReadTables(DataTable dataTable)
+        {
+            List<Teacher> teachers = new List<Teacher>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Teacher teacher = new Teacher()
+                {
+                    Number = (int)dr["lecturerid"],
+                    Name = dr["Name"].ToString()
+                };
+                teachers.Add(teacher);
+            }
+            return teachers;
+        }
+    }
+}
